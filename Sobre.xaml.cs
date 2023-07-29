25b216885081a6cfc6913b60b3a7f8e14051429b
Etec Profº Horácio Augusto da Silveira <gen_tms@hotmail.com>
2023-07-28 22:46:18 -0300

    first commit

---
+++ Sobre.xaml.cs
@@ -0,0 +1,16 @@
+namespace Notes;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
+
+}
